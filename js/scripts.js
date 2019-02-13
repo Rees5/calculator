234a90fde32719abd567687ea3feedd3cc42632e
Rees Alumasa <reesalumasa@gmail.com>
2019-02-13 10:35:40 +0300

    Add initial changes to calculator page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
-var add = function(number1, number2) {
-return number1 + number2;
+var number = function(number1, number2) {
+return number1, number2;
 }
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result= add(number1, number2);
-alert(result);
+var operator = function(operator) {
+return operator
+}
+var operator = prompt("Enter operator as either +, -, / or*");
+if(operator === "+") {
+	var add = function(number1, number2) {
+	return number1 + number2;
+	}
+	var result= add(number1, number2);
+	alert(result);
+} else if(operator === "-") {
+	var subtract = function(number1, number2) {
+	return number1 - number2;
+	}
+	var result= subtract(number1, number2);
+	alert(result);
+} else if(operator === "/") {
+	var divide = function(number1, number2) {
+	return number1 / number2;
+	}
+	var result= divide(number1, number2);
+	alert(result);
+} else {
+	var multiply = function(number1, number2) {
+	return number1 * number2;
+	}
+	var result= multiply(number1, number2);
+	alert(result);
+}
